deff6e35569b302159220e391e86b96723e38883
whitegreyblack <sangwoowhang@gmail.com>
2019-10-10 16:32:46 -0500

    Updated questions.py to use flags and entered text

--- questions.py
+++ questions.py
@@ -14,7 +14,11 @@ q_sort = False
 verbose = False
 show_answer = False
 file_import = "./questions.json"
-width, _ = os.get_terminal_size()
+
+width, height = os.get_terminal_size()
+y_offset = ((height - 24) // 2) if height > 24 else 0
+x_offset = ((width - 80) // 2) if width > 80 else 0
+x_indent = " " * x_offset
 
 class Question:
     question_id = 1
@@ -35,7 +39,9 @@ def print_results(questions):
     correct = sum(int(q.answered and q.correct) for q in questions)
     answered = sum(int(q.answered) for q in questions)
     total = len(questions)
+    clear_screen()
     if verbose:
+        print("Incorrect:")
         print('\n'.join(
             str(q.question_id)
                 for q in questions 
@@ -44,22 +50,29 @@ def print_results(questions):
     print(f"{correct}/{answered} questions ({correct/answered*100:.2f}%)")
     print(f"{correct}/{total} questions ({correct/total*100:.2f}%)")
 
-def ask_question(question):
+def ask_question(question, question_id):
     indent = "  "
+    qid = question.question_id if not q_sort else question_id
     text = [
-        f"{str(question.question_id)+'.' if i < 1 else indent} {s}"
+        f"{x_indent}{str(qid)+'.' if i<1 else indent} {s}"
             for i, s in enumerate(
-                textwrap.wrap(question.question, width-len(indent))
+                textwrap.wrap(question.question, 80-len(indent)-2)
             )
         ]
+    text.append('')
     # TODO: randomize answers within a question
     # save the full answer text to find index later after randomization
     # answer = question.answers[question.answer]
     # random.shuffle(question.answers)
     # question.answer = question.answers.indexof(answer)
     for i, answer in enumerate(question.answers):
-        text.append(f"    {chr(i+97)}. {answer}")
+        for j, s in enumerate(textwrap.wrap(answer, 80-len(indent*2)-2)):
+            text.append(f"{x_indent}{indent*2}{chr(i+97)+'.' if j<1 else '  '} {s}")
+        text.append('')
+    # output question and answer format
+    print('\n'.join(' ' for _ in range(y_offset)))
     print('\n'.join(text))
+    print()
 
 def build_valid_answer_set(question):
     return ''.join(chr(97+i) for i in range(len(question.answers)))
@@ -68,13 +81,13 @@ def check_valid_input(question, answer):
     answers = build_valid_answer_set(question)
     valid = all(ch in answers for ch in answer)
     if not valid:
-        print(f"Invalid input: must be in [{', '.join(answers)}]")
+        print(f"{x_indent}Invalid input: must be in [{', '.join(answers)}]")
     return valid
 
 def check_valid_answer_length(question, answer):
     valid = len(answer) == len(question.answer)
     if not valid:
-        print(f"Choose {len(question.answer)} answers")
+        print(f"{x_indent}Choose {len(question.answer)} answers")
     return valid
 
 def check_correct_answer(question, answer):
@@ -83,8 +96,9 @@ def check_correct_answer(question, answer):
 def handle_input(question):
     while True:
         try:
-            answer = input(">>> ")
+            answer = input(f"{x_indent}>>> ")
         except KeyboardInterrupt:
+            print()
             return None
         if (check_valid_input(question, answer) and
             check_valid_answer_length(question, answer)):
@@ -108,7 +122,14 @@ py questions.py -[s|v]
 
 if __name__ == "__main__":
     # system args parsing:
-    # ...
+    if sys.argv:
+        for arg in sys.argv:
+            if arg == '-s':
+                q_sort = True
+            elif arg == '-a':
+                show_answer = True
+            elif arg == '-v':
+                verbose = True
 
     # load the question set
     with open(file_import, "r") as f:
@@ -118,16 +139,23 @@ if __name__ == "__main__":
     # randomize order of questions
     if q_sort:
         random.shuffle(questions)
-    for q in questions:
+    for i, q in enumerate(questions):
         clear_screen()
-        ask_question(q)
+        ask_question(q, i+1)
         answer = handle_input(q)
         if not answer:
             break
         q.answered = True
         if check_correct_answer(q, answer):
             q.correct = True
-        elif show_answer:
-            print(', '.join(chr(97+a) for a in q.answer))
+        if show_answer:
+            if q.correct:
+                print(f"{x_indent}Correct")
+            else:
+                print(f"{x_indent}Incorrect: {', '.join(chr(97+a) for a in q.answer)}")
+            try:
+                input(f"{x_indent}Press any key to continue...")
+            except KeyboardInterrupt:
+                break
     print_results(questions)
 
