7a4fecd6b6179c3175dda201663b863bb8326bee
whitegreyblack <sangwoowhang@gmail.com>
2021-04-22 18:05:05 -0700

    Cleaning up model class and making it more readable with comments

--- model.py
+++ model.py
@@ -7,22 +7,30 @@ from dataclasses import dataclass, field
 
 
 class Question:
+
     question_id = 1
+
     def __init__(self, data, shuffle):
         self.question_id = Question.question_id
         Question.question_id += 1
         self.question = data['question_before']
         self.code = data['question_code']
         self.additional = data['question_after']
+
         if shuffle:
             random.shuffle(data['answers'])
+
+        # holds all possible answers to the question
         self.answers = [answer for answer, _ in data['answers']]
-        self.answer = [ 
-                i for i, (_, correct) in enumerate(data['answers']) 
+
+        # holds only the correct answer combination to the question
+        self.answer = [
+                i for i, (_, correct) in enumerate(data['answers'])
                     if correct
             ]
         self.answered = False
         self.correct = False
+
     def __repr__(self):
         return f"Question({self.question_id})"
 
@@ -35,19 +43,23 @@ class AnswerBuilder:
 
 @dataclass
 class QuestionBuilder:
+
     before: list = field(default_factory=list)
     after: list = field(default_factory=list)
     code: list = field(default_factory=list)
-    answers: list = field(default_factory=list)
+    answers: list[AnswerBuilder] = field(default_factory=list)
+
     def clear(self):
         self.before.clear()
         self.after.clear()
         self.code.clear()
         self.answers.clear()
+
     def set_correct_answers(self, answer: str):
         answers = [ord(a.strip()) - 97 for a in answer.split(',')]
         for i, answer in enumerate(self.answers):
             answer.correct = i in answers
+
     def serialize(self):
         return {
             'question_before': ' '.join(self.before),
